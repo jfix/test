0f385230fea50585757dd7b6abef874c6a1be2d4
Jakob Fix <jakob.fix@gmail.com>
2019-05-07 18:02:03 +0200

    Wow, this actually works!

--- auto-merge.js
+++ auto-merge.js
@@ -1,16 +1,26 @@
-const git = require('simple-git')('../test2')
+const gitWorkingDir = process.env.GITWD || '../test2'
+const git = require('simple-git')(gitWorkingDir)
 
-git.outputHandler((command, stdout, stderr) => {
-    stdout.pipe(process.stdout);
-    stderr.pipe(process.stderr);
-})
-.add('./*')
-.commit('commit before merging with master')
-.push('origin', 'develop')
-.checkout('master')
-.pull()
-.merge({'--strategy': 'ours'})
-.add('./*')
-.commit('merge develop with master')
-.push('origin', 'master')
-.checkout('develop')
+git
+    // make sure we see something while it works
+    .outputHandler((command, stdout, stderr) => {
+        stdout.pipe(process.stdout);
+        stderr.pipe(process.stderr);
+    })
+
+    // make sure we're in develop
+    .checkout('develop')
+    .add('./*')
+    .commit('commit before merging with master')
+    .push('origin', 'develop')
+
+    // move to master for merge
+    .checkout('master')
+    .pull()
+    .merge(['-srecursive', '-Xtheirs', 'develop'])
+    .add('./*')
+    .commit('merge develop with master')
+    .push('origin', 'master')
+
+    // switch back to develop
+    .checkout('develop')
